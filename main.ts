3830193a0c22d09b1d0896977420a09eab439a1b
RiekaPfitzner <87368343+RiekaPfitzner@users.noreply.github.com>
2023-06-20 22:31:13 +0200

    implement and optimize durchklicken function

--- main.ts
+++ main.ts
@@ -28,7 +28,18 @@ function wuerfeln () {
     basic.clearScreen()
 }
 function durchklicken () {
-	
+    led.plot(x_aktuell, y_aktuell)
+    if (x_aktuell == 2 || x_aktuell == 4) {
+        led.unplot(x_aktuell - 2, y_aktuell)
+    } else {
+        if (y_aktuell == 2 || y_aktuell == 4) {
+            led.unplot(4, y_aktuell - 2)
+        } else {
+            if (y_aktuell == 0) {
+                led.unplot(4, 4)
+            }
+        }
+    }
 }
 input.onGesture(Gesture.Shake, function () {
     if (wuerfeln_erlaubt) {
@@ -44,7 +55,32 @@ input.onButtonPressed(Button.AB, function () {
     bestaetigen()
 })
 input.onButtonPressed(Button.B, function () {
-    durchklicken()
+    if (enable_b) {
+        let spieler_b = 0
+        let spieler_a = 0
+        if (spieler_a) {
+            led.setBrightness(255)
+        }
+        if (spieler_b) {
+            led.setBrightness(255)
+        }
+        if (x_aktuell == 0 || x_aktuell == 2) {
+            durchklicken()
+            x_aktuell = x_aktuell + 2
+        } else {
+            if (x_aktuell == 4 && (y_aktuell == 0 || y_aktuell == 2)) {
+                durchklicken()
+                x_aktuell = 0
+                y_aktuell = y_aktuell + 2
+            } else {
+                if (x_aktuell == 4 && y_aktuell == 4) {
+                    durchklicken()
+                    x_aktuell = 0
+                    y_aktuell = 0
+                }
+            }
+        }
+    }
 })
 function start () {
     music.play(music.stringPlayable("C5 B A G G A B C5 ", 400), music.PlaybackMode.InBackground)
@@ -76,7 +112,11 @@ function start () {
 function main () {
     state = false
     enable_a = false
+    y_aktuell = 0
+    x_aktuell = 0
 }
+let y_aktuell = 0
+let x_aktuell = 0
 let enable_b = false
 let wuerfeln_erlaubt = false
 let enable_a = false
